d63a469f8fdf22d3fcd1acdfae83fda11af5c095
Ladd1993 <aladd1993@gmail.com>
2022-09-21 21:07:44 -0500

    changes

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,8 +2,6 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
 import { postRouter } from "./post";
 import { userRouter } from "./user";
 
@@ -11,8 +9,6 @@ export const appRouter = createRouter()
   .transformer(superjson)
   .merge("post.", postRouter)
   .merge("user.", userRouter)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
